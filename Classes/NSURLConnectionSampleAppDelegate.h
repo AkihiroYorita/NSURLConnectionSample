e4d60d168c05ad3fb91c3f45fbe46cf993fc1aa6
Akihiro Yorita <younglion3@yahoo.co.jp>
2010-08-24 16:31:29 +0900

    add Xcode project

---
+++ Classes/NSURLConnectionSampleAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  NSURLConnectionSampleAppDelegate.h
+//  NSURLConnectionSample
+//
+//  Created by いろは クリエイティブ on 10/08/24.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSURLConnectionSampleAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
